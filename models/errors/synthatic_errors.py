14d007237d15a5c5af50f73f133b8c2d9be01a86
João Victor Oliveira Couto <jictyvoo.ecomp@gmail.com>
2021-05-18 10:37:41 -0300

    :interrobang: Improve synthatic_errors

--- models/errors/synthatic_errors.py
+++ models/errors/synthatic_errors.py
@@ -3,17 +3,19 @@ from models.token import Token
 
 class SynthaticParseErrors:
 
-    def __init__(self, error: list[str], lexeme: Token):
+    def __init__(self, production_name: str, error: list[str], lexeme: Token):
+        self.__production_name = production_name
         self.error = error
         self.token = lexeme
 
     def __str__(self) -> str:
         if self.token:
-            return "Synthatic Error:" + str(self.token.get_line()) + ":" + str(
-                self.token.get_column()) + " >> Expected one of the following tokens " + str(self.error) + ": get " \
-                   + self.token.get_lexeme()
+            return "Synthatic Error on `%s`:%d:%d >> Expected one of the following tokens %s: get %s" % (
+                self.__production_name,
+                self.token.get_line(), self.token.get_column(), str(self.error), self.token.get_lexeme())
 
-        return "Synthatic Error:" + " >> Expected one of the following tokens " + str(self.error)
+        return "Synthatic Error on `%s`: >> Expected one of the following tokens %s" % (
+            self.__production_name, str(self.error))
 
     def __repr__(self) -> str:
         return self.__str__()
